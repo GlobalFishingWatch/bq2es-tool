9afb5fe43306b75247cb28855cef16b2fc6a1f8b
aperdizs <info@alvaroperdiz.com>
2020-07-21 11:01:27 +0200

    Remove unnecessary comment

--- main.go
+++ main.go
@@ -5,6 +5,3 @@ import "bq-to-es-cli/cmd"
 func main() {
 	cmd.Execute()
 }
-
-
-// ./bq-to-es-cli import --project-id=world-fishing-827 --query="SELECT * FROM scratch_megan.peru_track_data WHERE CAST(ssvid AS INT64) =  636091690" --elastic-search-url="https://alvaro@test@elastic.gfw.org"
