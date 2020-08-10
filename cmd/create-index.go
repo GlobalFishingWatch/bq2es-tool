484607b0cb5061aed5cceb7e79e750f5c1c540a5
aperdizs <info@alvaroperdiz.com>
2020-08-10 13:29:34 +0200

    Add create index command

--- cmd/create-index.go
+++ cmd/create-index.go
@@ -7,32 +7,31 @@ import (
 )
 
 func init() {
-	importCmd.Flags().StringVarP(&ProjectId,"project-id", "p", "", "Project id related to Cloud Storage bucket (required)")
-	importCmd.MarkFlagRequired("project-id")
-	importCmd.Flags().StringVarP(&BucketName,"bucket-name", "b", "", "Bucket name used to get mapping file (required)")
-	importCmd.MarkFlagRequired("bucket-name")
-	importCmd.Flags().StringVarP(&IndexName, "index-name", "i", "", "The name of the destination index (required)")
-	importCmd.MarkFlagRequired("index-name")
-	importCmd.Flags().StringVarP(&ElasticSearchUrl, "elastic-search-url", "u", "", "URL exposed by Elasticsearch cluster (required)")
-	importCmd.MarkFlagRequired("elastic-search-url")
-	rootCmd.AddCommand(importCmd)
+	createIndexCmd.Flags().StringVarP(&BucketName,"bucket-name", "b", "", "Bucket name used to get mapping file (required)")
+	createIndexCmd.MarkFlagRequired("bucket-name")
+	createIndexCmd.Flags().StringVarP(&IndName, "index-name", "i", "", "The name of the destination index (required)")
+	createIndexCmd.MarkFlagRequired("index-name")
+	createIndexCmd.Flags().StringVarP(&ESUrl, "elastic-search-url", "u", "", "URL exposed by Elasticsearch cluster (required)")
+	createIndexCmd.MarkFlagRequired("elastic-search-url")
+	rootCmd.AddCommand(createIndexCmd)
 }
 
-var ElasticSearchUrl string
-var ProjectId string
-var IndexName string
+var ProId string
 var BucketName string
+var IndName string
+var ESUrl string
 
 var createIndexCmd = &cobra.Command{
 	Use:   "create-index",
 	Short: "Create new index applying a custom mapping",
 	Long:  `Create new index applying a custom mapping
 Format:
-	bq2es create-index --project-id=[id] --bucket-name=[name] --index-name=[name] --elastic-search-url=[url]
+	bq2es create-index --bucket-name=[name] --index-name=[name] --elastic-search-url=[url]
 Example:
-	bq2es create-index --project-id=world-fishing-827 --bucket-name=elastic-search-mappings --index-name=test-vessels --elastic-search-url="https://user:password@elastic.gfw.org"`,
+	bq2es create-index --bucket-name=elastic-search-mappings --index-name=test-vessels --elastic-search-url="https://user:password@elastic.gfw.org"`,
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Println("→ Executing Create Index command")
+		action.CreateIndexWithCustomMapping(BucketName, IndName, ESUrl)
 	},
 }
 
--- internal/action/create-index.go
+++ internal/action/create-index.go
@@ -1,1 +1,81 @@
 package action
+
+import (
+	"cloud.google.com/go/storage"
+	"context"
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
+	"log"
+)
+
+func CreateIndexWithCustomMapping(bucketName string, indexName string, elasticSearchUrl string) {
+	var (
+		res *esapi.Response
+		err error
+	)
+	ctx := context.Background()
+
+	storageClient := getStorageClient(ctx)
+	defer storageClient.Close()
+
+	bucket := getBucket(ctx, storageClient, bucketName)
+	obj := bucket.Object(indexName + ".json")
+	reader, err := obj.NewReader(ctx)
+	if err != nil {
+		log.Fatalf("→ SG →→ Failed to get reader: %v", err)
+	}
+	defer reader.Close()
+
+	elasticClient := getEsClient(elasticSearchUrl)
+	createIndex(elasticClient, indexName)
+	res = putMapping(elasticClient, indexName, reader)
+	log.Printf("→ Set Mapping response: %v", res)
+}
+
+func getBucket(ctx context.Context, storageClient *storage.Client, bucketName string) *storage.BucketHandle {
+	bucket := storageClient.Bucket(bucketName)
+	attrs, err := bucket.Attrs(ctx)
+	if err != nil {
+		log.Fatalf("→ SG →→ Failed to get bucket: %v", err)
+	}
+	log.Printf("→ SG →→ bucket %s, created at %s, is located in %s with storage class %s\n",
+		attrs.Name, attrs.Created, attrs.Location, attrs.StorageClass)
+	return bucket
+}
+
+func getStorageClient(ctx context.Context) *storage.Client {
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("→ SC →→ Failed to create client: %v", err)
+	}
+	return client
+}
+
+func getEsClient(url string) *elasticsearch.Client {
+	cfg := elasticsearch.Config{
+		Addresses: []string{url},
+	}
+	es, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		log.Fatalf("→ SG →→ Error creating the client: %s", err)
+	}
+	return es
+}
+
+func createIndex(elasticClient *elasticsearch.Client, indexName string) {
+	res, err := elasticClient.Indices.Create(indexName)
+	if err != nil {
+		log.Fatalf("→ ES →→ Error creating the client: %s", err)
+	}
+	log.Printf("Set Mapping response: %v", res)
+}
+
+func putMapping(elasticClient *elasticsearch.Client, indexName string, reader *storage.Reader) *esapi.Response {
+	res, err := elasticClient.Indices.PutMapping(reader, func(index *esapi.IndicesPutMappingRequest) {
+		index.Index = []string{indexName}
+	})
+	if err != nil {
+		log.Fatalf("→ Error creating Index: %s", err)
+	}
+	return res
+}
